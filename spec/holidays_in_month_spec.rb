52a4629e5cfddd1e2f9a760e57ec23543527abab
Yuta Hasada <usgitan@gmail.com>
2016-03-12 20:58:20 +0900

    add spec

--- spec/holidays_in_month_spec.rb
+++ spec/holidays_in_month_spec.rb
@@ -1,46 +1,13 @@
 require './spec_helper'
 
-describe HolidaysInMonth do
-  describe '#holidays_in_month' do
-    context 'correct value' do
-      before do
-        @holidays = []
-        @month = 2
-        @year = 2016
-        @d = 0
-      end
-      it 'add holidays to array if wday is holidays in days in month' do
-        Time.days_in_month(@month, @year).times do
-          @d += 1
-          wday = Date.new(@year, @month, @d).wday
-          @holidays << "#{@year}-#{@month}-#{@d}" if wday == 0
-        end
-        expect(@holidays).to match(["2016-2-7", "2016-2-14", "2016-2-21", "2016-2-28"])
-      end
+describe Holidays do
+  subject(:get_in_month) { Holidays.get_in_month(month, year) }
+  describe '#get_in_month', '月の休日を返す' do
+    context '2016年の2月の場合' do
+      let(:month) { 2 }
+      let(:year) { 2016 }
+      let(:result) { ["2016-2-6", "2016-2-7", "2016-2-13", "2016-2-14", "2016-2-20", "2016-2-21", "2016-2-27", "2016-2-28", {"2016-02-11"=>"建国記念の日"}] }
+      it { is_expected.to eq result }
     end
-
-    #   context 'nothing month or days' do
-    #     it 'raises error'
-    #   end
   end
-  #
-  # describe '#add_holidays' do
-  #   context 'correct value' do
-  #     it 'return add holidays in array'
-  #   end
-  #
-  #   context 'nothing month or days' do
-  #     it 'raises error'
-  #   end
-  # end
-  #
-  # describe '#remove_holidays' do
-  #   context 'correct value' do
-  #     it 'return remove holidays in array'
-  #   end
-  #
-  #   context 'nothing month or days' do
-  #     it 'raises error'
-  #   end
-  # end
 end
