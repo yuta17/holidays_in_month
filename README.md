3dca75a83131bb5b210bf9583f7568242b028386
Yuta Hasada <usgitan@gmail.com>
2016-04-26 12:35:42 +0900

    fix

--- README.md
+++ README.md
@@ -2,46 +2,30 @@
 
 This gem is return holidays(Saturday, Sunday, National) of month in Japan.
 
-```ruby
-obj.get_in_month(2016, 1)
-=> ["2016-1-2", "2016-1-3", "2016-1-9", "2016-1-10", "2016-1-16", "2016-1-17",
+## Usage
+
+  ```ruby
+Holiday.new.in_month(2016, 1)
+  => ["2016-1-2", "2016-1-3", "2016-1-9", "2016-1-10", "2016-1-16", "2016-1-17",
   "2016-1-23", "2016-1-24", "2016-1-30", "2016-1-31", {"2016-01-01"=>"元日", "2016-01-11"=>"成人の日"}]
-```
+  ```
 
 ## Installation
 
-Add this line to your application's Gemfile:
+  Add this line to your application's Gemfile:
 
-```ruby
-gem 'holidays_in_month'
-```
+  ```ruby
+  gem 'holidays_in_month'
+  ```
 
-And then execute:
+  And then execute:
 
-    $ bundle install
+  $ bundle install
 
-Or install it yourself as:
+  Or install it yourself as:
 
-    $ gem install holidays_in_month
-
-## Usage
-
-Add this line to your application's Model or Controller:
-
-```ruby
-holidays = JP::Holidays.new
-holidays.get_in_month(year, month)
-```
-
-example:
-
-```ruby
-@holidays = JP::Holidays.new
-@holidays.get_in_month(2016, 1)
-@holidays => ["2016-1-2", "2016-1-3", "2016-1-9", "2016-1-10", "2016-1-16", "2016-1-17",
-  "2016-1-23", "2016-1-24", "2016-1-30", "2016-1-31", {"2016-01-01"=>"元日", "2016-01-11"=>"成人の日"}]
-```
+  $ gem install holidays_in_month
 
 ## License
 
-The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+  The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
--- lib/holidays_in_month.rb
+++ lib/holidays_in_month.rb
@@ -4,36 +4,34 @@ require 'active_support/time'
 require 'time'
 require 'yaml'
 
-module JP
-  class Holiday
-    def initialize
-      @nationaldays = {}
-      @holidays = []
+class Holiday
+  def initialize
+    @nationaldays = {}
+    @holidays = []
+  end
+
+  def in_month(year, month)
+    #祝日を取得して休日の配列に追加
+    holidays(year, month) << nationaldays.select { |n| n.match(/#{year}-#{"%02d" % month}-*/) }
+  end
+
+  private
+
+  def nationaldays
+    yaml = YAML.load_file(File.expand_path('./../../holidays.yml', __FILE__))
+    yaml.each do |date, name|
+      @nationaldays.store(date.strftime("%Y-%m-%d"), name)
     end
+    @nationaldays
+  end
 
-    def get_in_month(year, month)
-      #休日に月の祝日を追加
-      holidays(year, month) << nationaldays.select { |n| n.match(/#{year}-#{"%02d" % month}-*/) }
+  def holidays(year, month)
+    num_days_of_month = Date.new(year, month).end_of_month.day
+
+    1.upto(num_days_of_month) do |day|
+      wday = Date.new(year, month, day).wday
+      @holidays << "%04d-%02d-%02d" % [year, month, day] if wday == 6 || wday == 0
     end
-
-    private
-
-    def nationaldays
-      yaml = YAML.load_file(File.expand_path('./../../holidays.yml', __FILE__))
-      yaml.each do |date, name|
-        @nationaldays.store(date.strftime("%Y-%m-%d"), name)
-      end
-      @nationaldays
-    end
-
-    def holidays(year, month)
-      num_days_of_month = Date.new(year, month).end_of_month.day
-
-      1.upto(num_days_of_month) do |day|
-        wday = Date.new(year, month, day).wday
-        @holidays << "%04d-%02d-%02d" % [year, month, day] if wday == 6 || wday == 0
-      end
-      @holidays
-    end
+    @holidays
   end
 end
